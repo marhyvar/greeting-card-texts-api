ac5a51d5981f756a0e1574184fbeabce3d2757a9
M. Hyvarinen <54714032+marhyvar@users.noreply.github.com>
2020-05-01 21:20:57 +0300

    Lisää CRUD teksteihin

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ const tekstiRouter = require('./routes/teksti')
 const teemaRouter = require('./routes/teema');
 const express = require('express')
 const cors = require('cors')
-
+//const baseURL = '/api/v1'
 const app = express()
+//const router = express.Router()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 app.use('/api/v1/teemat', teemaRouter)
 app.use('/api/v1/tekstit', tekstiRouter)
+//app.use(baseURL, router)
 
 // Käynnistä palvelin
 app.listen(process.env.PORT || 3002, () => {

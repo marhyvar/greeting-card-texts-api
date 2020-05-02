971bd75b984c311400eacee61a4b6ec14b2df57f
M. Hyvarinen <54714032+marhyvar@users.noreply.github.com>
2020-05-02 13:43:16 +0300

    Muokkaa index.js

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ const tekstiRouter = require('./routes/teksti')
 const teemaRouter = require('./routes/teema');
 const express = require('express')
 const cors = require('cors')
-//const baseURL = '/api/v1'
+const baseURL = '/api/v1'
 const app = express()
-//const router = express.Router()
+const router = express.Router()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
-app.use('/api/v1/teemat', teemaRouter)
-app.use('/api/v1/tekstit', tekstiRouter)
-//app.use(baseURL, router)
+router.use('/teemat', teemaRouter)
+router.use('/tekstit', tekstiRouter)
+app.use(baseURL, router)
+app.get('/', (req, res) => {
+    res.json({ info: 'Express and PostgreSQL API'})
+})
 
 // Käynnistä palvelin
 app.listen(process.env.PORT || 3002, () => {

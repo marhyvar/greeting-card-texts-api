fc0039e9e7687d2566aecb51a5d151fe6c2d84c7
M. Hyvarinen <54714032+marhyvar@users.noreply.github.com>
2020-05-02 16:30:07 +0300

    Lisää rajoittimen kaikkiin pyyntöihin

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 
 const tekstiRouter = require('./routes/teksti')
-const teemaRouter = require('./routes/teema');
+const teemaRouter = require('./routes/teema')
 const express = require('express')
 const cors = require('cors')
+const rateLimit = require("express-rate-limit")
 const baseURL = '/api/v1'
 const app = express()
 const router = express.Router()
+const limiter = rateLimit({
+    windowMs: 5 * 60 * 1000, // 5 minuuttia
+    max: 40 // 40 pyyntöä per IP per 5 minuuttia
+})
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
+app.use(limiter)
 router.use('/teemat', teemaRouter)
 router.use('/tekstit', tekstiRouter)
 app.use(baseURL, router)

d464444578f22876a48a16d8d1edee1f69751c9f
M. Hyvarinen <54714032+marhyvar@users.noreply.github.com>
2020-05-02 16:00:07 +0300

    Lisää validointi pyyntöihin

--- routes/teema.js
+++ routes/teema.js
@@ -1,17 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const { pool } = require('../config')
+const { check, validationResult } = require('express-validator');
 
-const haeTeemat = (request, response) => {
+//hae teemat
+router.get('/', (request, response) => {
     pool.query('SELECT * FROM teemat', (error, results) => {
         if (error) {
             throw error
         }
         response.status(200).json(results.rows)
     })
-}
+})
 
-const lisaaTeema = (request, response) => {
+//lisää teema
+router.post('/', [
+    check('teema')
+    .not().isEmpty()
+    .isLength({ min: 2, max: 100})
+    .trim()
+    .escape()
+], (request, response) => {
+    const errors = validationResult(request)
+    if (!errors.isEmpty()) {
+        return response.status(422).json({ errors: errors.array() })
+    }
     const teema = request.body.teema 
     pool.query('INSERT INTO teemat (teema) VALUES ($1)', [teema], (error, results) => {
         if (error) {
@@ -19,9 +32,10 @@ const lisaaTeema = (request, response) => {
         }
         response.status(201).send('Teema luotu')
     })
-}
+})
 
-const haeTekstitTeemalla = (request, response) => {
+// hae samaan teemaan kuuluvat tekstit
+router.get('/:id/tekstit', (request, response) => {
     const id = parseInt(request.params.id)
     pool.query('SELECT * FROM tekstit WHERE teema_id = $1', [id], (error, results) => {
         if (error) {
@@ -29,10 +43,6 @@ const haeTekstitTeemalla = (request, response) => {
         }
         response.status(200).json(results.rows)
     })
-}
-
-router.get('/', haeTeemat)
-router.post('/', lisaaTeema)
-router.get('/:id/tekstit', haeTekstitTeemalla)
+})
 
 module.exports = router

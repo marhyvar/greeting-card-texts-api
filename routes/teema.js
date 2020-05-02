bfffef2b4deb3fd763411ee561f4597c76815227
M. Hyvarinen <54714032+marhyvar@users.noreply.github.com>
2020-05-02 12:46:23 +0300

    Lisää endpointteja teemaan

--- routes/teema.js
+++ routes/teema.js
@@ -11,6 +11,28 @@ const haeTeemat = (request, response) => {
     })
 }
 
+const lisaaTeema = (request, response) => {
+    const teema = request.body.teema 
+    pool.query('INSERT INTO teemat (teema) VALUES ($1)', [teema], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(201).send('Teema luotu')
+    })
+}
+
+const haeTekstitTeemalla = (request, response) => {
+    const id = parseInt(request.params.id)
+    pool.query('SELECT * FROM tekstit WHERE teema_id = $1', [id], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
 router.get('/', haeTeemat)
+router.post('/', lisaaTeema)
+router.get('/:id/tekstit', haeTekstitTeemalla)
 
 module.exports = router
